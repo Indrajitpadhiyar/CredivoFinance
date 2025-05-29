bfb6c1d6403c74a7699bbd7452ffb933ff3d650d
indrajit <indrajitpadhiyar107@gmail.com>
2025-05-29 22:21:15 +0530

    make home page and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 // import Login from './components/login-signup/Login';
 import Navbar from './components/Navbar/navbar';
+import Home from './components/pages/Home';
+import Signup from './components/login-signup/Signup';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,11 +25,13 @@ function App() {
   return (
     <>
       <div className='cursor w-5 h-5 bg-red-500 blur rounded-full absolute z-10 '></div>
-      <div className='w-full h-screen relative overflow-hidden'>
+      <div className='w-full h-screen relative overflow-hidden '>
         <div className="circule1 absolute w-[25%] h-[50%] -left-[120px] -top-[120px] bg-blue-400 rounded-full blur-3xl"></div>
         <div className="circule2 absolute w-[25%] h-[50%] -right-[120px] -bottom-[100px] bg-blue-400 rounded-full blur-3xl"></div>
         {/* <Login /> */}
-        <Navbar />
+        {/* <Navbar />
+        <Home /> */}
+        <Signup />
       </div>
     </>
   )
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,9 +1,53 @@
+import { useGSAP } from '@gsap/react';
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
+import gsap from 'gsap';
 
 const Home = () => {
+  useGSAP(() => {
+    gsap.from('.text1', {
+      opacity: 0,
+      duration: 1,
+      x: -100,
+    })
+    gsap.from('.text2', {
+      opacity: 0,
+      duration: 1,
+      x: 100,
+    })
+    gsap.from('.text3', {
+      opacity: 0,
+      duration: 1,
+      x: -100,
+    })
+    
+  })
   return (
-    <div>
-      hello world
+    <div className='flex w-full h-screen justify-center items-center flex-col '>
+      <div className='w-[80%] h-[35%]  flex  justify-center'>
+        <h1 className='text-7xl font-thin flex gap-3 flex-col'>
+          <span className='text1'>
+            THE BEST
+          </span>
+          <span className='text2 text-red-400 flex flex-col ml-20'>
+            INVESTMENT
+          </span>
+          <span className='text3 mr-20 text-red-400'>PLATFORM</span>
+        </h1>
+      </div>
+      <h2 className='text-2xl mt-10 '>
+        <TypeAnimation
+          sequence={[
+            'Grow your wealth with smart ,secure , and simplified investing— trusted by thousands.',
+            100,
+          ]}
+          wrapper="span"
+          repeat={Infinity}
+          style={{ fontSize: '1em', display: 'inline-block' }}
+          className='text-2xl '
+        />
+      </h2>
+      <button className='bg-orange-500 font-bold text-white px-4 py-2 rounded-md hover:bg-red-600 mt-10'>JOIN NOW</button>
     </div>
   )
 }
