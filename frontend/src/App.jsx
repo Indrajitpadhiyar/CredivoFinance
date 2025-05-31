0382547239ce25e2f11f8ea207af103cb5ad71d0
Drumil Thakor <shinehonda009@gmail.com>
2025-05-31 22:29:55 +0530

    Your descriptive message about these frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,17 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 // import Login from './components/login-signup/Login';
 import Navbar from './components/Navbar/navbar';
+// import Signup from './components/login-signup/Signup';
+// import Home from './components/pages/Home';
+import About from './components/pages/About';
+
 
 function App() {
   const [count, setCount] = useState(0)
   const [mousePointer, setMousePointer] = useState({ x: 0, y: 0 })
 
   useGSAP(() => {
+
     const mouseMove = (e) => {
       gsap.to('.cursor', {
         x: e.clientX,
@@ -23,11 +28,14 @@ function App() {
   return (
     <>
       <div className='cursor w-5 h-5 bg-red-500 blur rounded-full absolute z-10'></div>
-      <div className='w-full h-screen relative overflow-hidden'>
+      <div className='w-full h-screen relative overflow-y-auto'>
         <div className="circule1 absolute w-[25%] h-[50%] -left-[120px] -top-[120px] bg-blue-400 rounded-full blur-3xl"></div>
         <div className="circule2 absolute w-[25%] h-[50%] -right-[120px] -bottom-[100px] bg-blue-400 rounded-full blur-3xl"></div>
         {/* <Login /> */}
+        {/* <Signup /> */}
+        {/* <Home /> */}
         <Navbar />
+        <About />
       </div>
     </>
   )
