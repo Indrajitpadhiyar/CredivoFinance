eccf0b3b6c63534eb2281882d326ecb1631f5fdd
indrajit <indrajitpadhiyar107@gmail.com>
2025-06-04 14:27:54 +0530

    add animation

--- frontend/src/components/pages/DashBoard/Profile.jsx
+++ frontend/src/components/pages/DashBoard/Profile.jsx
@@ -5,6 +5,8 @@ import { UserContext } from '../../../context/useContext';
 import { useUserAuth } from '../../../components/Hooks/UseUSerAuth';
 // import { API_BASE_URL } from '../../../utils/axiosinstance';
 import { API_BASE_URL } from "../../../utils/apiPath";
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../../assets/motion';
 
 const Profile = () => {
 
@@ -30,11 +32,18 @@ const Profile = () => {
     clearUser();
     navigate("/login");
   };
-
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.2, // control line-by-line delay
+      },
+    },
+  };
 
   return (
-    <div className='flex h-screen w-full gap-2 items-center p-2'>
-      <div className="h-[97vh] w-[20%] rounded-2xl shadow-[0_4px_10px_rgba(0,0,0,0.3)] backdrop-blur-md bg-white/30 z-20">
+    <div className='flex h-screen w-full gap-2 items-center p-2 '>
+      <div className="h-[97vh] w-[20%] rounded-2xl flex flex-col overflow-hidden items-center shadow-[0_4px_10px_rgba(0,0,0,0.3)] backdrop-blur-md bg-white/30 z-20">
         <div className=''>
           {/* <h5>
             {user?.fullName || "User Name"}
@@ -42,24 +51,32 @@ const Profile = () => {
           <div>
 
             {user?.profileImageUrl ? (
-              <img
+              <motion.img
+                variants={fadeIn("down", 0.1)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: true }}
                 src={`${API_BASE_URL}${encodeURI(user?.profileImageUrl)}`}
                 alt="Profile"
-                className='w-40 m-5 rounded-full  bg-amber-400 h-40 object-cover rounded-tr-2xl'
+                className='w-40  mt-10 bg-amber-400 h-40 object-cover  rounded-bl-4xl rounded-tr-4xl'
               />
 
             ) : <></>}
 
 
             {SIDE_MANU_DATA.map((item) => (
-              <button
+              <motion.button
                 key={item.id}
-                className='w-full flex items-center gap-2 p-2 hover:bg-gray-200 rounded-lg'
+                variants={fadeIn("right", item.id * 0.4)} // add delay based on index
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: true }}
+                className='w-full flex mt-5 items-center gap-2 p-2 hover:bg-gray-200 rounded-lg'
                 onClick={() => hacdleClick(item.path)}
               >
                 <item.icon className='text-xl' />
                 <span>{item.lable}</span>
-              </button>
+              </motion.button>
             ))}
           </div>
         </div>
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,14 +1,13 @@
-import {
-  LuLayoutDashboard,
-  LuWalletMinimal,
-  LuLogOut, 
-} from "react-icons/lu";
+import { LuLayoutDashboard, LuWalletMinimal, LuLogOut } from "react-icons/lu";
+import { FaHome } from "react-icons/fa";
+// import { FaPiggyBank } from "react-icons/fa";
+import { FaCoins } from "react-icons/fa";  
 
 export const SIDE_MANU_DATA = [
   {
     id: "01",
     lable: "Dashboard",
-    icon:LuLayoutDashboard,
+    icon: LuLayoutDashboard,
     path: "/dashboard",
   },
   {
@@ -17,10 +16,21 @@ export const SIDE_MANU_DATA = [
     icon: LuWalletMinimal,
     path: "/income",
   },
-    {
-        id: "03",
-        lable: "Logout",
-        icon: LuLogOut,
-        path: "Logout",
-    },
+  {
+    id: "03",
+    lable: "Home",
+    icon: FaHome,
+    path: "/Home",
+  },
+ {    id: "04",
+    lable: "Investment",
+    icon: FaCoins,
+    path: "/Investment",
+  },
+  {
+    id: "05",
+    lable: "Logout",
+    icon: LuLogOut,
+    path: "Logout",
+  },
 ];
