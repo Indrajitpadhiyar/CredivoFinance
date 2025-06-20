80d19b6bae88415ade3ecfce27cbf62a1e010f4c
INDRAJIT <145698455+Indrajitpadhiyar@users.noreply.github.com>
2025-06-20 22:08:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import gsap from 'gsap';
 import Login from './components/login-signup/Login';
 import Profile from './components/pages/DashBoard/dashBoard/Profile';
 import Register from './components/login-signup/Register'; 
-import UserProvider from './context/useContext';
+import UserProvider from './context/UserProvider';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
 import Expense from './components/pages/Expense';
