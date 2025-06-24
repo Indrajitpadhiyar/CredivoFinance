bd13dd869c600a4b62c9f88cd805bbd956ca6453
indrajit <indrajitpadhiyar107@gmail.com>
2025-06-24 10:44:46 +0530

    Fix: force case rename for Render

---
+++ frontend/src/components/pages/DashBoard/dashBoard/Last30DaysExpense.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import CustomeBarChart from '../Chart/CustomeBarChart'
+import { useState, useEffect } from 'react'
+import { preparExpenseBarChartData } from '../../../../utils/helper'
+
+const Last30DaysExpense = ({ transactions }) => {
+    const [ChartData, setChartData] = useState([]);
+
+    useEffect(() => {
+        const result = preparExpenseBarChartData(transactions || []);
+        setChartData(result);
+    }, [transactions]);
+
+    return (
+        <div className='card col-span-1'>
+            <div className='flex items-center justify-between'>
+                <h5 className='text-lg font-semibold'>Last30DaysExpense</h5>
+            </div>
+            <CustomeBarChart data={ChartData} />
+        </div>
+    )
+}
+
+export default Last30DaysExpense
