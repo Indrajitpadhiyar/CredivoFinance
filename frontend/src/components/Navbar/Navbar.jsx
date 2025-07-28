61e14844afa7e7277082d5bdcf1a802421b265bc
indrajit <indrajitpadhiyar107@gmail.com>
2025-07-28 21:19:19 +0530

    add responsive navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
             <div className="md:hidden flex items-center">
               <button onClick={toggleSidebar}>
                 <FaBars size={24} />
-              </button>
+              </button> 
             </div>
           </div>
         </div>
