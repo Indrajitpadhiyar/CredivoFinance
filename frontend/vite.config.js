bff9e77fde7cd06b9e0b8d021d5146ec4f4182c9
indrajit <indrajitpadhiyar107@gmail.com>
2025-08-23 13:37:11 +0530

    add vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // ✅ Add this line
+import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy"; // ✅ Add this
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects", // ✅ yaha se lega
+          dest: "."                 // ✅ dist/ me daalega
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // ✅ Add this
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
